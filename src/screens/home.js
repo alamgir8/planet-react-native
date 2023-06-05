9f82bf5304e9d6e6c7c40eff7a48a99957033c6b
alamgir8 <ahossain38900@gmail.com>
2023-06-05 21:21:36 +0600

    feat(v-1): updated search handler

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,6 @@
 import { FlatList, StyleSheet, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import React, { useState } from "react";
 import PlanetHeader from "../components/common/planet_header";
 import { colors } from "../theme/color";
 import { planet_list } from "../data/planet_list";
@@ -8,8 +8,15 @@ import { spacing } from "../theme/spacing";
 import PlanetItem from "../components/common/planet_item";
 
 const Home = ({ navigation }) => {
+  const [planetList, setPlanetList] = useState(planet_list);
   const renderItem = ({ item }) => {
     return <PlanetItem item={item} />;
+  };
+  const handleFilterPlanet = (text) => {
+    const filteredList = planet_list.filter((item) =>
+      item.name.toLowerCase().includes(text.toLowerCase())
+    );
+    setPlanetList(filteredList);
   };
   return (
     <SafeAreaView style={styles.container}>
@@ -18,10 +25,11 @@ const Home = ({ navigation }) => {
         placeholder="Type the planet name"
         placeholderTextColor={colors.white}
         style={styles.searchInput}
+        onChangeText={(text) => handleFilterPlanet(text)}
       />
       <FlatList
         contentContainerStyle={styles.list}
-        data={planet_list}
+        data={planetList}
         keyExtractor={(item) => item.name}
         renderItem={renderItem}
         ItemSeparatorComponent={() => <View style={styles.separator} />}
